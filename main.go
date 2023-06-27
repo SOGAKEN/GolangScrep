afde95306097ef358fe54668f399680a04d626d8
SOGAKEN <sogaken0919@gmail.com>
2023-06-27 22:13:25 +0900

    all Csv

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
@@ -17,38 +19,29 @@ type Result struct {
 }
 
 func Scrape(keyword string) []Result {
-	// Google search URL
 	googleURL := "http://www.google.com/search?q=" + keyword
 
-	// Create HTTP client
 	client := &http.Client{}
 
-	// Create HTTP request
 	req, _ := http.NewRequest("GET", googleURL, nil)
 
-	// Set user agent
 	req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36")
 
-	// Execute HTTP request
 	resp, _ := client.Do(req)
 
-	// Create goquery document from HTTP response
 	document, _ := goquery.NewDocumentFromReader(resp.Body)
 
-	// Slice to store results
 	results := []Result{}
 
-	// Find search results
 	sel := document.Find("div.yuRUbf")
 
-	// For each item, extract title and URL
 	for i := range sel.Nodes {
 		item := sel.Eq(i)
 		title := item.Find("h3").Text()
 		link, _ := item.Find("a").Attr("href")
 		link = strings.TrimPrefix(link, "/url?q=")
 
-		if i < 5 { // only top 5
+		if i < 5 {
 			results = append(results, Result{Keyword: keyword, Title: title, URL: link})
 		} else {
 			break
@@ -59,38 +52,53 @@ func Scrape(keyword string) []Result {
 }
 
 func main() {
-	// Open the file
-	csvfile, err := os.Open("keywords.csv")
+	files, err := ioutil.ReadDir("./")
 	if err != nil {
 		fmt.Println(err)
-		return
+		os.Exit(1)
 	}
-	defer csvfile.Close()
 
-	reader := csv.NewReader(csvfile)
-	// reading all rows at once
-	keywordLines, err := reader.ReadAll()
+	for _, file := range files {
+		if filepath.Ext(file.Name()) == ".csv" {
+			// Open the file
+			csvfile, err := os.Open(file.Name())
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			defer csvfile.Close()
 
-	// Create output file
-	outfile, err := os.Create("results.csv")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer outfile.Close()
+			reader := csv.NewReader(csvfile)
+			// reading all rows at once
+			keywordLines, err := reader.ReadAll()
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
 
-	writer := csv.NewWriter(outfile)
-	defer writer.Flush()
+			// Create output file
+			outfileName := strings.TrimSuffix(file.Name(), filepath.Ext(file.Name())) + "_results.csv"
+			outfile, err := os.Create(outfileName)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			defer outfile.Close()
 
-	// Write header
-	writer.Write([]string{"Keyword", "Title", "URL"})
+			writer := csv.NewWriter(outfile)
+			defer writer.Flush()
 
-	for _, line := range keywordLines {
-		keyword := line[0]
-		results := Scrape(keyword)
+			// Write header
+			writer.Write([]string{"Keyword", "Title", "URL"})
 
-		for _, result := range results {
-			writer.Write([]string{result.Keyword, result.Title, result.URL})
+			for _, line := range keywordLines {
+				keyword := line[0]
+				results := Scrape(keyword)
+
+				for _, result := range results {
+					writer.Write([]string{result.Keyword, result.Title, result.URL})
+				}
+			}
 		}
 	}
 }
